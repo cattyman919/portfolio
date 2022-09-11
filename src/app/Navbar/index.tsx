fb8b071e9ccaf9eaeead8d5aaffda870058a1830
Cattyman <88530324+cattyman919@users.noreply.github.com>
2022-09-11 21:32:29 +0700

    Added Profile content

--- src/app/Navbar/index.tsx
+++ src/app/Navbar/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between w-full p-3 bg-cyberpunk-5">
+    <div className="flex items-center justify-between w-full p-3 bg-cyberpunk-5 ">
       <div>
         <h2 className="m-0 text-cyberpunk-3">LOGO</h2>
       </div>
       <div className="flex items-center justify-between gap-5 mr-10 ">
-        <h2 className="m-0 border-4 rounded border-cyberpunk-1 text-cyberpunk-3">
+        <h2 className="m-0 border-0 border-b-4 cursor-pointer hover:border-solid border-cyberpunk-1 text-cyberpunk-3">
           Profile
         </h2>
-        <h2 className="m-0 text-cyberpunk-3">Projects</h2>
-        <h2 className="m-0 text-cyberpunk-3">Skills</h2>
-        <h2 className="m-0 text-cyberpunk-3">Posts</h2>
+        <h2 className="m-0 border-0 border-b-4 cursor-pointer hover:border-solid border-cyberpunk-1 text-cyberpunk-3">
+          Projects
+        </h2>
+        <h2 className="m-0 border-0 border-b-4 cursor-pointer hover:border-solid border-cyberpunk-1 text-cyberpunk-3">
+          Skills
+        </h2>
       </div>
     </div>
   );
--- src/app/Profile/index.tsx
+++ src/app/Profile/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-cyberpunk-4">
+      <h2>Gambar Muka</h2>
+      <p>
+        Hey my name is Seno Pamungkas Rahman and im a frontend developer and
+        also a game developer
+      </p>
+    </div>
+  );
 };
 
 export { Profile };
