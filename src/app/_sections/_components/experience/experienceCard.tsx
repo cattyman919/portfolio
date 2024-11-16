74fd6d7552b9a04a26a3dc99f3439127446f1350
Cattyman <88530324+cattyman919@users.noreply.github.com>
2024-11-16 19:41:23 +0700

    [fix] experience mobile view

--- src/app/_sections/_components/experience/experienceCard.tsx
+++ src/app/_sections/_components/experience/experienceCard.tsx
@@ -13,24 +13,24 @@ const ExperienceCard = forwardRef(function ExperienceCard(
       ref={ref}
       className="border-2 border-primary-accent [&_svg]:open:rotate-180  open:shadow-lg open:shadow-primary-accent transition-all  w-[90%] lg:w-[85%] h-full   bg-secondary-bg p-4 rounded-3xl"
     >
-      <summary className="flex items-center flex-col cursor-pointer  md:flex-row gap-5 rounded-3xl lg:min-h-[150px]">
-        <Link
-          href={props.company_url}
-          className="w-full h-[100px] md:w-[150px] md:h-fit  shrink-0 "
-          target="_blank"
-        >
-          <Image
-            src={props.image}
-            className="rounded-lg object-contain w-full h-full lg:object-contain    "
-            alt="company logo"
-          />
-        </Link>
-        <div className="flex flex-col   justify-start gap-5">
+      <summary className="flex flex-row justify-between items-center  cursor-pointer  gap-3   rounded-3xl lg:min-h-[150px]">
+        <div className="flex flex-col gap-3 md:flex-row md:items-center md:gap-5">
+          <Link
+            href={props.company_url}
+            className="w-[100px] md:w-[150px] h-fit  shrink-0 "
+            target="_blank"
+          >
+            <Image
+              src={props.image}
+              className="rounded-lg object-contain w-full h-full lg:object-contain    "
+              alt="company logo"
+            />
+          </Link>
           <div className="flex flex-col gap-3">
-            <h3 className="text-2xl font-bold">
+            <h3 className=" text-sm md:text-xl lg:text-2xl font-bold">
               {props.position} @ {props.company}
             </h3>
-            <p className="font-bold md:text-lg">
+            <p className="text-sm font-bold md:text-lg">
               {props.start_date} - {props.end_date}
             </p>
             <div className="flex gap-2 relative">
@@ -47,13 +47,14 @@ const ExperienceCard = forwardRef(function ExperienceCard(
             </div>
           </div>
         </div>
+
         <MdKeyboardArrowDown
-          className="ml-auto mr-5 transition-transform"
+          className="md:ml-auto w-[32px] h-[32px] md:w-[64px] md:h-[64px] md:mr-5 transition-transform"
           size={64}
           color="#000000"
         />
       </summary>
-      <p className="text-base text-justify xl:text-lg/10 mt-5 ">
+      <p className="text-base/10 text-justify xl:text-lg/10 mt-5 ">
         {props.description}
       </p>
     </details>
