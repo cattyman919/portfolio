321899746d7e87c5b3b3245ed8f339805b3bacec
Cattyman <88530324+cattyman919@users.noreply.github.com>
2024-12-23 18:46:55 +0700

    update link to blog

--- src/app/_sections/home.tsx
+++ src/app/_sections/home.tsx
@@ -127,7 +127,7 @@ const Home = forwardRef(function Home(props, ref: LegacyRef<HTMLElement>) {
               <p>Resume</p>
             </Link>
             <Link
-              href="https://blog.seno-dev.com"
+              href="https://blog.seno-dev.com/blog"
               target="_blank"
               className="min-w-[100px] flex flex-col items-center text-center mt-5 lg:mt-0 bg-primary-accent text-white font-bold p-2 rounded-lg hover:scale-110 hover:bg-white hover:border-2 hover:border-primary-accent hover:text-primary-accent transition-all "
             >
