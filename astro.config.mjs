49b523b5669e79ca21ec4b4afc47f85993d3dcbb
Seno Pamungkas <senop.rahman@gmail.com>
2026-03-15 02:29:46 +0700

    initial commit new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+export default defineConfig({});
--- src/types/skill.ts
+++
@@ -1,6 +0,0 @@
-import type { AstroComponentFactory } from "astro/runtime/server/index.js";
-export interface SkillCardProp {
-  logo: AstroComponentFactory;
-  title: string;
-  rating: number;
-}
